ab4ffa48423fdd9b4d9eb7f47e0082a95e3346d3
Blank <wangfj11@foxmail.com>
2024-09-10 19:23:03 +0800

    [修改]1. 修改数据分析

--- docs/.vitepress/theme/components/Analytics.vue
+++ docs/.vitepress/theme/components/Analytics.vue
@@ -1,14 +1,11 @@
 <script setup>
-// var _hmt = _hmt || [];
-// (function () {
-//   var hm = document.createElement("script");
-//   hm.src = "https://hm.baidu.com/hm.js?bb98fe196aa7f73b90177c872e5644ab";
-//   var s = document.getElementsByTagName("script")[0];
-//   s.parentNode.insertBefore(hm, s);
-// })();
-
+const umami = document.createElement("script");
+umami.src = "https://cloud.umami.is/script.js";
+umami.setAttribute("data-website-id", "92a468c9-7c0e-4e55-bea3-0e3c3bb0330e");
+umami.defer = true
+const s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(umami, s);
 </script>
-<script defer src="https://cloud.umami.is/script.js" data-website-id="92a468c9-7c0e-4e55-bea3-0e3c3bb0330e"></script>
 <template>
 </template>
 
